758d5c200403aa43b18774ecee672e6c57b389e8
Andrew Wilder <andrew.m.wilder@gmail.com>
2014-10-14 14:58:53 -0400

    Refactored code

--- src/SecLogin.java
+++ src/SecLogin.java
@@ -1,57 +1,25 @@
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.math.BigInteger;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 import java.security.*;
-import java.math.*;
-import java.crypto.*;
+
+import javax.crypto.Cipher;
+import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
 public class SecLogin implements Runnable {
-	
-	/** Socket for the server to communicate with a client */
-	private static Socket clientSocket = null;
-	
+
 	/** Size of user history files */
 	private static final int HIST_SIZE = 20;
-	
-	/** Number of distinquishing features per user */
+
+	/** Number of distinguishing features per user */
 	private static final int DIST_FEAT_CNT = 15;
-	
+
 	/** Random used for generating values */
 	private static Random rand = new Random();
-	
-	/**
-	 * Main client function
-	 * Connect to a server, authenticate
-	 * @param args Contains destination IP in args[0], and port in args[1]
-	 */
-	private static void clientMain(String[] args) {
-		try {
-			// Connect to the server
-			Socket serverSocket = new Socket(args[0], Integer.parseInt(args[1]));
-			PrintWriter writeSock = new PrintWriter(serverSocket.getOutputStream(), true);
-			BufferedReader readSock = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
-			
-			// Send login request to server
-			System.out.printf("username: ");
-			Scanner input = new Scanner(System.in);
-			String loginRequest = input.next();
-			writeSock.write(loginRequest);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-	}
 
 	/**
 	 * Calculate the polynomial given coefficients
@@ -73,55 +41,68 @@ public class SecLogin implements Runnable {
 
 		return a0;
 	}
-	
-	private BigInteger calculate_hash(String password , int q, int x) {
-		// HmacSHA1 for G(x) - Key is the password (user input) supplied in file  
-					try {
-				        Mac mac = Mac.getInstance("HmacSHA1");
-				        SecretKeySpec secret=new SecretKeySpec(password.getBytes(),mac.getAlgorithm());
-				        mac.init(secret);
-				     //   String input = new String("2");
-				        String input = toString(x);
-				        
-				        byte[] rawHmac = mac.doFinal(input.getBytes());
-				        BigInteger Alpha1 = new BigInteger(1,rawHmac);
-				        
-				        System.out.println("Hash value is " +Alpha1);
-						}
-						catch (Exception e) {
-							System.out.println("Exception" + e);
-						}
-			        
-					Alpha1 = Alpha1.mod(q);
-			        return Alpha1;
-			        
-	}
-	
-	
-	private void Encrypt_Alpha_Beta(BigInteger[] Alpha, BigInteger[] Beta, String password) {
-		
+
+	/**
+	 * Perform Gr(x) mod q.
+	 * @param password What we are encrypting wityh SHA-1
+	 * @param q The modulus of the size we are allowing the output to be
+	 * @param x Seed for the SHA-1 digest
+	 * @return The completed calculation of Gr(x) mod q
+	 */
+	private BigInteger calculate_hash(String password , BigInteger q, int x) {
+		// HmacSHA1 for G(x) - Key is the password (user input) supplied in file 
+		BigInteger Alpha1 = null;
 		try {
-		    MessageDigest md = MessageDigest.getInstance("SHA1");
-		    
-		    md.update(password.getBytes()); 
-		 	byte[] output = md.digest();
-		    output = Arrays.copyOf(output,16);
-		    
-		    String encrypt_value = new String ("prabhendu");
-		    
-		    SecretKeySpec secretkeyspec = new SecretKeySpec(output,"AES");
-		    Cipher cipher = Cipher.getInstance("AES");
-		    cipher.init(Cipher.ENCRYPT_MODE, secretkeyspec);
-		    byte[] encrypted = cipher.doFinal((encrypt_value.getBytes()));
-		    BigInteger test3 = new BigInteger(1,output);
-		    System.out.println("encrypted string: " + test3 + "-" + output);
-		 } 
+			Mac mac = Mac.getInstance("HmacSHA1");
+			SecretKeySpec secret=new SecretKeySpec(password.getBytes(),mac.getAlgorithm());
+			mac.init(secret);
+			//   String input = new String("2");
+			String input =  "" + x;
+
+			byte[] rawHmac = mac.doFinal(input.getBytes());
+			Alpha1 = new BigInteger(1,rawHmac);
+
+			System.out.println("Hash value is " +Alpha1);
+		}
 		catch (Exception e) {
-		    System.out.println("Exception: "+e);
-		 }		
-		
+			System.out.println("Exception" + e);
+		}
+
+		Alpha1 = Alpha1.mod(q);
+		return Alpha1;
+
 	}
-	
+
+	/**
+	 * Encrypt alpha and beta arrays
+	 * @param Alpha Alpha array
+	 * @param Beta Beta array
+	 * @param password User's password, used as the encryption key
+	 */
+	private void Encrypt_Alpha_Beta(BigInteger[] Alpha, BigInteger[] Beta, String password) {
+
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA1");
+
+			md.update(password.getBytes()); 
+			byte[] output = md.digest();
+			output = Arrays.copyOf(output,16);
+
+			String encrypt_value = new String ("prabhendu");
+
+			SecretKeySpec secretkeyspec = new SecretKeySpec(output,"AES");
+			Cipher cipher = Cipher.getInstance("AES");
+			cipher.init(Cipher.ENCRYPT_MODE, secretkeyspec);
+			byte[] encrypted = cipher.doFinal((encrypt_value.getBytes()));
+			BigInteger test3 = new BigInteger(1,output);
+			System.out.println("encrypted string: " + test3 + "-" + output);
+		} 
+		catch (Exception e) {
+			System.out.println("Exception: "+e);
+		}		
+
+	}
+
 	/**
 	 * Attempt to log in
 	 * @param credentials CSV of the login credentials and feature vector
@@ -137,16 +118,16 @@ public class SecLogin implements Runnable {
 		for(int i = 0; i < DIST_FEAT_CNT; ++i) {
 			features[i] = Integer.parseInt(vals[i + 2]);
 		}
-		
+
 		// Get pasword from the user
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter password for user " + name);
 		String password = in.nextLine();
-		
-		
+
+
 		// If new user, do init
 		if(!new File(name + ".hist").exists()) {
-			
+
 			// Create random vector
 			BigInteger q = new BigInteger(160, Integer.MAX_VALUE, new Random());
 			BigInteger hpwd = new BigInteger(256, Integer.MAX_VALUE, new Random()).mod(q);
@@ -154,43 +135,41 @@ public class SecLogin implements Runnable {
 			for(int i = 0; i < DIST_FEAT_CNT - 1; ++i) {
 				coeff[i] = Math.abs(rand.nextInt());
 			}
-			
-			Biginteger y = new BigInteger;
-			BigInteger Alpha1 = new BigInteger;
-			BigInteger Beta1 = new BigInteger;
+
+			BigInteger y, Alpha1, Beta1;
 			BigInteger[] Alpha = new BigInteger[DIST_FEAT_CNT];
 			BigInteger[] Beta = new BigInteger[DIST_FEAT_CNT];
-			
-//Calculation of Alpha values for instruction table				
+
+			//Calculation of Alpha values for instruction table				
 			for(int i = 1; i <= DIST_FEAT_CNT ; ++i ) {
-			    y = calculatePoly(hpwd, coeff, 2*i);
+				y = calculatePoly(hpwd, coeff, 2*i);
 				Alpha1 = calculate_hash(password, q, 2*i);
 				Alpha1 = Alpha1.add(y);
 				Alpha[i-1] = Alpha1;
 			}
-			
-//Calculation of Beta values for instruction table
+
+			//Calculation of Beta values for instruction table
 			for(int i = 1; i <= DIST_FEAT_CNT ; ++i ) {
-			    y = calculatePoly(hpwd, coeff, 2*i + 1);
+				y = calculatePoly(hpwd, coeff, 2*i + 1);
 				Beta1 = calculate_hash(password, q, 2*i + 1);
 				Beta1 = Beta1.add(y);
 				Beta[i-1] = Beta1;
 			}
-		    
-			
-			Encrypt_alpha_beta(Alpha, Beta, password);
-	        
+
+
+			Encrypt_Alpha_Beta(Alpha, Beta, password);
+
 		}
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * Server function. It will have already connected to a client
 	 */
 	@Override
 	public void run() {
-		
+
 		Scanner inputFile = null;
 		try {
 			inputFile = new Scanner(new File("testfile.txt"));
@@ -198,130 +177,19 @@ public class SecLogin implements Runnable {
 			e.printStackTrace();
 			System.exit(1);
 		}
-		
+
 		while(inputFile.hasNext()) {
 			String nextLine = inputFile.nextLine();
 			boolean attempt = loginAttempt(nextLine);
 			System.out.println("Attempted login: " + (attempt ? "accept" : "failed"));
 		}
 	}
-	
-	/**
-	 * Create a new SecLogin server object
-	 * @param clientSocket The client who has already been connected to
-	 */
-	private SecLogin(Socket clientSocket) {
-		this.clientSocket = clientSocket;
-	}
-	
-	/**
-	 * Main server function
-	 * Listen on a port, create a new thread per user
-	 * @param args Contains the port number in args[1]
-	 */
-	private static void serverMain(String[] args) {
-		
-		new Thread(new SecLogin(null)).start();
-		boolean b1 = false, b2 = false;
-		if(b1 == b2) {
-			return;
-		}
-		
-		// Initialize server socket to listen on a port
-		ServerSocket serverSocket = null;
-		try {
-			serverSocket = new ServerSocket(Integer.parseInt(args[1]));
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-		
-		// Never stop accepting connections
-		while(true) {
-			
-			// Create new thread per connecting user
-			Socket clientSocket = null;
-			try {
-				clientSocket = serverSocket.accept();
-			} catch (IOException e) {
-				e.printStackTrace();
-				System.exit(1);
-			}
-			new Thread(new SecLogin(clientSocket)).start();
-		}
-	}
-	
-	/**
-	 * Print details for failed program usage
-	 * @param msg The message about the error
-	 * @param details Details of the error
-	 */
-	private static void printErr(String msg, String details) {
-		System.err.printf("%s: %s\n", msg, details);
-		System.err.println("Usage:   java SecLogin <IP | -s> <port>");
-		System.err.println("Example: java SecLogin 123.64.244.15 60005 (client)");
-		System.err.println("         java SecLogin -s 60005            (server)");
-		System.exit(1);
-	}
-	
-	/**
-	 * Make sure the user put in the command line arguments correctly
-	 * @param args The command line arguments passed from main
-	 */
-	private static void validateArgs(String[] args) {
-		
-		// First, check for the correct number of arguments (2)
-		if(args.length != 2) {
-			printErr("Incorrect number of args", args.length + " args");
-		
-		// Next, if this is a client, check the IP format
-		} else if(!args[0].equals("-s")) {
-			boolean invalidIP = false;
-			if(!Pattern.matches("\\d+.\\d+.\\d+.\\d+", args[0])) {
-				invalidIP = true;
-			} else {
-				String[] IPfields = args[0].split("\\.");
-				for(int i = 0; i < 4; ++i) {
-					int n = Integer.parseInt(IPfields[i]);
-					if(n < 0x00 || n > 0xFF) {
-						invalidIP = true;
-					}
-				}
-			}
-			if(invalidIP) {
-				printErr("Invalid IP", args[0]);
-			}
-		}
-		
-		// Finally, check the port format
-		boolean invalidPort = false;
-		if(!Pattern.matches("\\d+", args[1])) {
-			invalidPort = true;
-		} else {
-			int n = Integer.parseInt(args[1]);
-			if(n < 0x0000 || n > 0xFFFF) {
-				invalidPort = true;
-			}
-		}
-		if(invalidPort) {
-			printErr("Invalid port", args[1]);
-		}
-	}
-	
+
 	/**
 	 * Validate user input, and call the client or server main for SecLogin
 	 * @param args Contains IP and port for client, or -s and port for server
 	 */
 	public static void main(String[] args) {
-		
-		// Make sure the arguments were typed in correctly
-		validateArgs(args);
-		
-		// Run client or server main
-		if(args[0].equals("-s")) {
-			serverMain(args);
-		} else {
-			clientMain(args);
-		}
+		new Thread(new SecLogin()).start();
 	}
 }
